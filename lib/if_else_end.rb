0916da9346edf1c7d57248d202180a0f9896417d
Mark De La Rosa <mark.p.dlr@gmail.com>
2020-03-31 20:17:11 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,6 +1,3 @@
 # Write your solution here
-puts "You know what year it is??"
-this_year = Time.now.year
-puts "Hey, it's 2019!" if this_year == 2019
-#this_year = Time.now.year
-puts "Hey, it's not 2019!" unless this_year == 2019
+current_time = Time.now
+puts current_time
